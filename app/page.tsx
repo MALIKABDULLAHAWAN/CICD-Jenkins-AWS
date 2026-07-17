4dce432370e68f0d95d73afaf9b64bcc35f8e438
MALIKABDULAHAWAN <u2022274@giki.edu.pk>
2026-07-18 01:20:21 +0500

    5

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs about me
+            Read our docs about me yo
           </a>
         </div>
       </main>
